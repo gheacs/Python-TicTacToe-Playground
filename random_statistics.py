63aa49c90eaf894ce76c1cd296a5185129162826
Ghea Chrestella Suyono <132538718+gheacs@users.noreply.github.com>
2023-12-03 19:25:17 -0800

    Update random_statistics.py
    Include the board position for first player movement

--- random_statistics.py
+++ random_statistics.py
@@ -7,6 +7,10 @@ def generate_random_game_statistics():
         'TotalMoves': random.randint(10, 50)
     }
 
+# Function to convert row and column to a 1-9 representation
+def convert_to_1_9(row, col):
+    return row * 3 + col + 1
+
 # Generate random game data for 40 games
 game_data = []
 
@@ -16,18 +20,26 @@ for _ in range(40):
     game_duration = round(random.uniform(10, 120), 2)
     game_stats = generate_random_game_statistics()
     
+    # Generate random row and column for the first move
+    row = random.randint(0, 2)
+    col = random.randint(0, 2)
+
+    # Convert row and column to a 1-9 representation
+    first_move = convert_to_1_9(row, col)
+    
     game_data.append([
         first_player,
         winner,
         'Bronze',
         game_duration,
-        game_stats['TotalMoves']
+        game_stats['TotalMoves'],
+        first_move  # Include the first move in the data
     ])
 
 # Write the game data to a CSV file
 with open('random_game_data.csv', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['First Player', 'Winner', 'Rank', 'Game Duration (s)', 'Total Moves'])
+    csv_writer.writerow(['First Player', 'Winner', 'Rank', 'Game Duration (s)', 'Total Moves', 'First Move (1-9)'])
     csv_writer.writerows(game_data)
 
-print("Random game data with 'First Player' and 'Winner' statistics generated and saved to 'random_game_data.csv'.")
+print("Random game data with 'First Player', 'Winner', and 'First Move' statistics generated and saved to 'random_game_data.csv'.")
