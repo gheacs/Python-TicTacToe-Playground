231cfbb884c4bcc27f3fd4ece973a83ef68f6bcb
Ghea Chrestella Suyono <132538718+gheacs@users.noreply.github.com>
2023-12-03 17:46:07 -0800

    Update random_statistics.py
    Make changes to add several column on generated csv file: First player, winner

--- random_statistics.py
+++ random_statistics.py
@@ -11,11 +11,13 @@ def generate_random_game_statistics():
 game_data = []
 
 for _ in range(40):
-    winner = random.choice(['X', 'O'])
+    first_player = random.choice(['X', 'O'])
+    winner = random.choice([first_player, 'Draw'])
     game_duration = round(random.uniform(10, 120), 2)
     game_stats = generate_random_game_statistics()
     
     game_data.append([
+        first_player,
         winner,
         'Bronze',
         game_duration,
@@ -25,7 +27,7 @@ for _ in range(40):
 # Write the game data to a CSV file
 with open('random_game_data.csv', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['Winner', 'Rank', 'Game Duration (s)', 'Total Moves'])
+    csv_writer.writerow(['First Player', 'Winner', 'Rank', 'Game Duration (s)', 'Total Moves'])
     csv_writer.writerows(game_data)
 
-print("Random game data with 'Total Moves' statistic generated and saved to 'random_game_data.csv'.")
+print("Random game data with 'First Player' and 'Winner' statistics generated and saved to 'random_game_data.csv'.")
